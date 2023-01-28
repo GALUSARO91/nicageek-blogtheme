1196a8e1218d0ad40646e2b4740865198925ae0f
Luis Rodriguez <glsr91@hotmail.com>
2023-01-27 22:00:47 -0600

    comment form first attempt

--- single.php
+++ single.php
@@ -19,29 +19,37 @@
 <?php rewind_posts();?>
     </section>
 <section>
-            <div class="carousel-nav">
-                <ul class="pagination">
-                    <?php while(have_posts()){ the_post();?>
-                        <?php previous_post_link(
-                            '<li class="page-item page-link">   
-                            <span aria-hidden="true">%link</span>
-                    </li>')?>
-                        <?php next_post_link(
-                             '<li class="page-item page-link">   
-                             <span aria-hidden="true">%link</span>
-                         </li>')?>
-     
-                    <?php } ?>
-                    <!-- <li class="page-item page-link">   
-                        <span aria-hidden="true">&laquo;</span>
-                    </li>
+    <div class="carousel-nav">
+        <ul class="pagination">
+            <?php while(have_posts()){ the_post();?>
+                <?php previous_post_link(
+                    '<li class="page-item page-link">   
+                    <span aria-hidden="true">%link</span>
+                </li>','Previous Post')?>
+                <?php next_post_link(
+                        '<li class="page-item page-link">   
+                        <span aria-hidden="true">%link</span>
+                    </li>','Next Post')?>
+            <?php } ?>
+        </ul>
+    </div>
+</section>
 
-                    <li class="page-item page-link"> 
-                        <span aria-hidden="true">&raquo;</span>
-                    </li> -->
-                </ul>
-            </div>
-    </section>
+    <?php 
+        $commentbox_args=[
+            'class_container' => 'comment-form',
+            'class_form' => '',
+            'class_submit'=>'btn btn-outline-success',
+            'label_submit' => 'Enviar',
+            'comment_field' => '<div class="row my-3 justify-content-center">
+            <textarea class="col-8" name="comment" id="" cols="4" rows="10" placeholder="Write comment!!!"></textarea>
+            </div>',
+            'comment_notes_after' => '<div class="row my-3 gx-5 justify-content-end pe-5">',
+            'submit_field' => '<div class ="col-2">%1$s</div><div class ="col-2">%2$s</div></div>'
+        ];
+        comment_form($commentbox_args)
+    ;?>
+
 
 
 <?php get_footer();?>
