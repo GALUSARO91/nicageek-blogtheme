48116a4239442f03c2d7ff3960770014a815b033
galusarom <glsr91@hotmail.com>
2023-05-09 20:23:34 -0600

    page styles updated

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
 <?php if(have_posts()){ ?>
 
  
-     <section class="latest-post">
+     <section>
 
             <?php while(have_posts()){ the_post();?>
                 <h2><?php the_title();?></h2>
